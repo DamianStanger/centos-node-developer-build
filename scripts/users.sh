cfcd15fbf752410c500e4433ca802e6fdb36fe0f
Damian Stanger <Damian.Stanger+github@gmail.com>
2016-12-05 22:30:12 +0000

    adding config for the user names and passwords

--- scripts/users.sh
+++ scripts/users.sh
@@ -2,24 +2,24 @@
 
 echo "***** Running users *****"
 
-user_name = jq '.users.user.name' ../config.json
-user_password = jq '.users.user.password' ../config.json
-vagrant_name = jq '.users.vagrant.name' ../config.json
-vagrant_password = jq '.users.vagrant.password' ../config.json
-root_name = jq '.users.root.name' ../config.json
-root_password = jq '.users.root.password' ../config.json
-echo #{user_name}
-echo #{user_password}
+user_name=$(jq '.users.user.name' ../config.json)
+user_password=$(jq '.users.user.password' ../config.json)
+vagrant_name=$(jq '.users.vagrant.name' ../config.json)
+vagrant_password=$(jq '.users.vagrant.password' ../config.json)
+root_name=$(jq '.users.root.name' ../config.json)
+root_password=$(jq '.users.root.password' ../config.json)
+echo user_name = $user_name
+echo user_password = $user_password
 
 echo "*** ammending vagrant user password ***"
-echo '{vagrant_name}:#{vagrant_password}' | chpasswd 
+echo '$vagrant_name:$vagrant_password' | chpasswd 
 
-echo "*** adding custom user '#{user_name}' ***"
-sudo useradd #{user_name}
-sudo echo #{user_password} | passwd #{user_name} --stdin
-sudo gpasswd -a #{user_name} wheel
+echo "*** adding custom user '$user_name' ***"
+sudo useradd $user_name
+sudo echo $user_password | passwd $user_name --stdin
+sudo gpasswd -a $user_name wheel
 
 echo "*** ammending root password ***"
-echo '#{root_name}:#{root_password}' | chpasswd 
+echo '$root_name:$root_password' | chpasswd 
 
 echo "***** Fin! users *****"
